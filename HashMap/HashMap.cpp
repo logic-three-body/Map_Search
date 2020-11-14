ae41413163bcdf51b4cb271539a5607af9e88057
logic-three-body <1552451580@qq.com>
2020-11-14 11:44:34 +0800

    Learn HashMap with Open_Adress

--- HashMap/HashMap.cpp
+++ HashMap/HashMap.cpp
@@ -1,20 +1,120 @@
-﻿// HashMap.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include<iostream>
+#include<stdlib.h>
+#include<cmath>
+#define MAXTABLESIZE 100000   // 定义允许开辟的最大散列表长度 
+typedef int Index;
+typedef int ElementType;
+typedef Index Position;
+typedef enum {   // 分别对应：有合法元素、空、有已删除元素 
+	Legitimate, Empty, Deleted
+} EntryType;  // 定义单元状态类型 
 
-#include <iostream>
+typedef struct HashEntry Cell;
+struct HashEntry {   //  哈希表存值单元 
+	ElementType Data;  // 存放元素
+	EntryType Info;  // 单元状态	
+};
 
-int main()
-{
-    std::cout << "Hello World!\n";
+typedef struct HashTbl *HashTable;
+struct HashTbl {  // 哈希表结构体 
+	int TableSize;   // 哈希表大小 
+	Cell *Cells;   // 哈希表存值单元数组 
+};
+
+using namespace std;
+
+int NextPrime(int N);  // 查找素数 
+HashTable CreateTable(int TableSize); // 创建哈希表 
+Index Hash(int Key, int TableSize);   // 哈希函数 
+
+// 查找素数 
+int NextPrime(int N) {
+	int p = (N % 2) ? N + 2 : N + 1;  // 从大于 N 的下个奇数开始
+	int i;
+
+	while (p <= MAXTABLESIZE) {
+		for (i = (int)sqrt(p); i > 2; i--)
+			if (!(p%i))  // p 不是素数 
+				break;
+		if (i == 2)
+			break;
+		p += 2;  // 继续试探下个奇数 
+	}
+	return p;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+// 创建哈希表 
+HashTable CreateTable(int TableSize) {
+	HashTable H;
+	int i;
+	H = (HashTable)malloc(sizeof(struct HashTbl));
+	// 保证哈希表最大长度是素数 
+	H->TableSize = NextPrime(TableSize);
+	// 初始化单元数组
+	H->Cells = (Cell *)malloc(sizeof(Cell)*H->TableSize);
+	// 初始化单元数组状态 
+	for (int i = 0; i < H->TableSize; i++)
+		H->Cells[i].Info = Empty;
+	return H;
+}
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+// 平方探测查找 
+Position Find(HashTable H, ElementType Key) {
+	Position CurrentPos, NewPos;
+	int CNum = 0;   // 记录冲突次数
+	CurrentPos = NewPos = Hash(Key, H->TableSize);
+	// 如果当前单元状态不为空，且数值不等，则一直做 
+	while (H->Cells[NewPos].Info != Empty && H->Cells[NewPos].Data != Key) {
+		if (++CNum % 2) { // 冲突奇数次发生 
+			NewPos = CurrentPos + (CNum + 1) / 2 * (CNum + 1) / 2;
+			// 如果越界，一直减直到再次进入边界 
+			while (H->TableSize <= NewPos) {
+				NewPos -= H->TableSize;
+			}
+		}
+		else {  // 冲突偶数次发生 
+			NewPos = CurrentPos - CNum / 2 * CNum / 2;
+			// 如果越界，一直加直到再次进入边界 
+			while (NewPos < 0) {
+				NewPos += H->TableSize;
+			}
+		}
+	}
+	return NewPos;
+}
+
+// 插入
+bool Insert(HashTable H, ElementType Key, int i) {
+	Position Pos = i;
+	Pos = Find(H, Key);
+	// 如果单元格状态不是"存在合法元素" 
+	if (H->Cells[Pos].Info != Legitimate) {
+		H->Cells[Pos].Info = Legitimate;
+		H->Cells[Pos].Data = Key;
+	}
+	return true;
+}
+
+// 除留余数法哈希函数 
+Index Hash(int Key, int TableSize) {
+	return Key % TableSize;
+}
+
+
+void output(HashTable H) {
+	for (int i = 0; i < H->TableSize; i++)
+		cout << i << " " << H->Cells[i].Data << endl;
+}
+
+int main() {
+	HashTable H = CreateTable(9);
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		cin >> tmp;
+		Insert(H, tmp, i);
+	}
+	output(H);
+	return 0;
+}
